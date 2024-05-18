a44835f4f0502b9552faeac7a1563f1ab146d62c
ivansindacco <56919658+ivansindacco@users.noreply.github.com>
2024-05-18 12:03:56 +0300

    Add files via upload

---
+++ стратегия_3.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+
+class Strategy {
+public:
+    virtual void executeStrategy() = 0;
+};
+
+class ConcreteStrategyA : public Strategy {
+public:
+    void executeStrategy() override {
+        std::cout << "Executing Strategy A" << std::endl;
+    }
+};
+
+class ConcreteStrategyB : public Strategy {
+public:
+    void executeStrategy() override {
+        std::cout << "Executing Strategy B" << std::endl;
+    }
+};
+
+class Context {
+    Strategy* strategy;
+
+public:
+    Context(Strategy* strategy) : strategy(strategy) {}
+
+    void executeStrategy() {
+        strategy->executeStrategy();
+    }
+};
+
+int main() {
+    Strategy* strategyA = new ConcreteStrategyA();
+    Strategy* strategyB = new ConcreteStrategyB();
+
+    Context contextA(strategyA);
+    Context contextB(strategyB);
+
+    contextA.executeStrategy();
+    contextB.executeStrategy();
+
+    delete strategyA;
+    delete strategyB;
+
+    return 0;
+}
---
+++ шаблонныйметод_3.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <string>
+
+class AbstractClass {
+public:
+    void templateMethod() {
+        stepOne();
+        stepTwo();
+        stepThree();
+    }
+
+    virtual void stepOne() = 0;
+    virtual void stepTwo() = 0;
+
+    void stepThree() {
+        std::cout << "Sleep" << std::endl;
+    }
+};
+
+class ConcreteClass1 : public AbstractClass {
+public:
+    void stepOne() override {
+        std::cout << "ConcreteClass1: Study" << std::endl;
+    }
+
+    void stepTwo() override {
+        std::cout << "ConcreteClass1: Eat" << std::endl;
+    }
+};
+
+class ConcreteClass2 : public AbstractClass {
+public:
+    void stepOne() override {
+        std::cout << "ConcreteClass2: Work" << std::endl;
+    }
+
+    void stepTwo() override {
+        std::cout << "ConcreteClass2: Eat" << std::endl;
+    }
+};
+
+int main() {
+    AbstractClass* class1 = new ConcreteClass1();
+    class1->templateMethod();
+    delete class1;
+
+    AbstractClass* class2 = new ConcreteClass2();
+    class2->templateMethod();
+    delete class2;
+
+    return 0;
+}
